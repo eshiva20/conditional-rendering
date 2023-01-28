056993773b24189c1f93ca33c7d5e6a8fd140ece
shiva <shiva@nimapinfotech.com>
2023-01-28 18:51:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [toggle,setToggle]=useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {toggle? <h1>Lights ON</h1>: <h1>Lights OFF</h1> }
+    <button  onClick={()=>setToggle(!toggle)}> Click </button>
+    </>
   );
 }
 
